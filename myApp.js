8a56cc1d226a883c1881f087d1b0c5372bd6a6f5
Tim Tersigni <tim.tersigni@gmail.com>
2020-08-02 03:39:51 -0500

    Get input from client - Route parameters

--- myApp.js
+++ myApp.js
@@ -49,15 +49,17 @@ var app = express();
 // })
 
 /** 8) Chaining middleware. A Time server */
-app.get('/now', (req, res, next) => {
-  req.time = new Date().toString();
-  next()
-}, (req, res) => {
-  res.json({ time: req.time });
-})
+// app.get('/now', (req, res, next) => {
+//   req.time = new Date().toString();
+//   next()
+// }, (req, res) => {
+//   res.json({ time: req.time });
+// })
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', (req, res) => {
+  res.json({ echo: req.params.word})
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
